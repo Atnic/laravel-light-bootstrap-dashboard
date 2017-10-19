8493beae4dc1a557e6e22d5562604a0ad41fd1e6
Farid Inawan <frdteknikelektro@gmail.com>
2017-10-19 07:21:59 +0700

    Optimizing js, so it will not be error when 'use strict'

--- resources/assets/js/light-bootstrap-dashboard.js
+++ resources/assets/js/light-bootstrap-dashboard.js
@@ -91,7 +91,7 @@ var fixedTop = false;
 var navbar_initialized = false;
 
 $(document).ready(function() {
-  window_width = $(window).width();
+  var window_width = $(window).width();
 
   // check if there is an image set for the sidebar's background
   lbd.checkSidebarImage();
@@ -132,42 +132,42 @@ $(window).resize(function() {
   }
 });
 
-lbd = {
+window.lbd = {
   misc: {
     navbar_menu_visible: 0
   },
 
   checkSidebarImage: function() {
-    $sidebar = $('.sidebar');
-    image_src = $sidebar.data('image');
+    var $sidebar = $('.sidebar');
+    var image_src = $sidebar.data('image');
 
-    if (image_src !== undefined) {
-      sidebar_container = '<div class="sidebar-background" style="background-image: url(' + image_src + ') "/>'
+    if (image_src !== 'undefined') {
+      var sidebar_container = '<div class="sidebar-background" style="background-image: url(' + image_src + ') "/>'
       $sidebar.append(sidebar_container);
     }
   },
   initRightMenu: function() {
-    if (!navbar_initialized) {
-      $navbar = $('nav').find('.navbar-collapse').first().clone(true);
+    if (!this.navbar_initialized) {
+      var $navbar = $('nav').find('.navbar-collapse').first().clone(true);
 
-      $sidebar = $('.sidebar');
-      sidebar_color = $sidebar.data('color');
+      var $sidebar = $('.sidebar');
+      var sidebar_color = $sidebar.data('color');
 
-      $logo = $sidebar.find('.logo').first();
-      logo_content = $logo[0].outerHTML;
+      var $logo = $sidebar.find('.logo').first();
+      var logo_content = $logo[0].outerHTML;
 
-      ul_content = '';
+      var ul_content = '';
 
       $navbar.attr('data-color', sidebar_color);
 
       //add the content from the regular header to the right menu
       $navbar.children('ul').each(function() {
-        content_buff = $(this).html();
+        var content_buff = $(this).html();
         ul_content = ul_content + content_buff;
       });
 
       // add the content from the sidebar to the right menu
-      content_buff = $sidebar.find('.nav').html();
+      var content_buff = $sidebar.find('.nav').html();
       ul_content = ul_content + content_buff;
 
 
@@ -177,26 +177,27 @@ lbd = {
         '</ul>' +
         '</div>';
 
-      navbar_content = logo_content + ul_content;
+      var navbar_content = logo_content + ul_content;
 
       $navbar.html(navbar_content);
 
       $('body').append($navbar);
 
-      background_image = $sidebar.data('image');
-      if (background_image != undefined) {
+      var background_image = $sidebar.data('image');
+      if (background_image != 'undefined') {
         $navbar.css('background', "url('" + background_image + "')")
           .removeAttr('data-nav-image')
           .addClass('has-image');
       }
 
 
-      $toggle = $('.navbar-toggle');
+      var $toggle = $('.navbar-toggle');
 
       $navbar.find('a').removeClass('btn btn-round btn-default');
       $navbar.find('button').removeClass('btn-round btn-fill btn-info btn-primary btn-success btn-danger btn-warning btn-neutral');
       $navbar.find('button').addClass('btn-simple btn-block');
 
+      var lbd = this;
       $toggle.click(function() {
         if (lbd.misc.navbar_menu_visible == 1) {
           $('html').removeClass('nav-open');
@@ -211,7 +212,7 @@ lbd = {
             $toggle.addClass('toggled');
           }, 430);
 
-          div = '<div id="bodyClick"></div>';
+          var div = '<div id="bodyClick"></div>';
           $(div).appendTo("body").click(function() {
             $('html').removeClass('nav-open');
             lbd.misc.navbar_menu_visible = 0;
@@ -226,12 +227,10 @@ lbd = {
 
         }
       });
-      navbar_initialized = true;
+      this.navbar_initialized = true;
     }
-
   }
 }
-
 
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
